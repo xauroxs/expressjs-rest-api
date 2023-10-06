278e99ae843daf455859ca44b0484ababca11bad
xauroxs <xauroxs@gmail.com>
2023-10-06 10:40:35 +0300

    add delete user method & route, create middleware to check if user is owner

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUsersAction } from "../db/users";
+import { deleteUserAction, getUsersAction } from "../db/users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -10,6 +10,23 @@ export const getAllUsers = async (
     const users = await getUsersAction();
 
     return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserAction(id);
+
+    return res.json(deletedUser);
   } catch (error) {
     console.log(error);
 
