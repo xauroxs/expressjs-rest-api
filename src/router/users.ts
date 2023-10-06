a5758a274c2fe16e4c733d04a4c1a64d2543c99c
xauroxs <xauroxs@gmail.com>
2023-10-06 10:10:12 +0300

    create middleware for checking if user is authenticated

--- src/router/users.ts
+++ src/router/users.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 import { getAllUsers } from "../controllers/users";
 
+import { isAuthenticated } from "../middlewares";
+
 export default (router: express.Router) => {
-  router.get("/users", getAllUsers);
+  router.get("/users", isAuthenticated, getAllUsers);
 };
